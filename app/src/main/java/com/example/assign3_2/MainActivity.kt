367b382c5d4139751c14a9805a84ff4f4170c4de
Wyatt <wnapier@bu.edu>
2025-09-24 00:03:28 -0400

    basic functionality

---
+++ app/src/main/java/com/example/assign3_2/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.assign3_2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.assign3_2.ui.theme.Assign3_2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Assign3_2Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ProfilePicture(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ProfilePicture(
+    modifier: Modifier = Modifier
+) {
+    var showBadge by remember { mutableStateOf(false) }
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = modifier
+                .size(50.dp)
+                .clip(CircleShape)
+                .background(Color.Gray)
+        )
+        {if (showBadge) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .size(18.dp)
+                    .clip(CircleShape)
+                    .background(Color.Red)
+            )
+        }}
+        Button(
+            modifier = Modifier,
+            content = { Text(text = "Toggle Badge") },
+            onClick = { showBadge = !showBadge }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfilePicturePreview() {
+    Assign3_2Theme {
+        ProfilePicture(modifier = Modifier.padding(20.dp))
+    }
+}
